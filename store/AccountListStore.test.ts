0b1a2b87b3d831bd6254230faa86b13bc371e325
Akhilesh Thite <akhithite@gmail.com>
2023-08-30 00:58:25 +0530

    add tests for list method in AccountListStore

--- store/AccountListStore.test.ts
+++ store/AccountListStore.test.ts
@@ -29,3 +29,17 @@ test('AccountListStore - remove patterns', async t => {
   t.false(await store.matches('@user2@example.com')) // Wildcard was removed
   t.true(await store.matches('@user1@example.com'))
 })
+
+test('AccountListStore - list patterns', async t => {
+  const store = newAccountListStore()
+  const patterns = ['@user1@example.com', '@user2@example.com']
+  await store.add(patterns)
+
+  let accounts = await store.list()
+  t.deepEqual(accounts.sort(), patterns.sort(), 'All patterns should be listed after addition')
+
+  await store.remove(['@user1@example.com'])
+  accounts = await store.list()
+
+  t.deepEqual(accounts, ['@user2@example.com'], 'Only @user2@example.com should remain after removal of @user1@example.com')
+})
