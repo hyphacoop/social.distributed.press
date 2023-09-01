9ade378139563bf2974ee02ff2f6030dfeaa5303
Mauve Signweaver <contact@mauve.moe>
2023-09-01 11:59:01 -0400

    Log more details on signature fail

--- api/inbox.ts
+++ api/inbox.ts
@@ -62,7 +62,7 @@ export const inboxRoutes = (cfg: APIConfig, store: Store, apsystem: ActivityPubS
     // TODO: check that the actor is the one that signed the request
     const activity = request.body
 
-    if (activity.actor !== submittedActor) throw new Error('Submitted activity must be from signed actor')
+    if (activity.actor !== submittedActor) throw new Error(`Submitted activity must be from signed actor. Activity: ${activity.actor} Request: ${submittedActor}`)
 
     await apsystem.ingestActivity(actor, activity)
 
