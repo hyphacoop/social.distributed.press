136033331f788e7244bb02739f3251c76a5a7508
Akhilesh Thite <akhithite@gmail.com>
2023-11-18 00:19:49 +0530

    feat: implement client APIs for social inbox

--- src/client/index.ts
+++ src/client/index.ts
@@ -19,10 +19,11 @@ const TYPE_JSON = 'application/json'
 const GET = 'GET'
 const POST = 'POST'
 const DELETE = 'DELETE'
+const PUT = 'PUT'
 
 const NEWLINE = '\n'
 
-type VALID_METHODS = typeof GET | typeof POST | typeof DELETE
+type VALID_METHODS = typeof GET | typeof POST | typeof PUT | typeof DELETE
 
 type VALID_TYPES = typeof TYPE_TEXT | typeof TYPE_JSON | typeof TYPE_LDJSON | undefined
 
@@ -66,6 +67,7 @@ export class SocialInboxClient {
     return response
   }
 
+  // Actorinfo
   async setActorInfo (actor: string, info: ActorInfo): Promise<void> {
     await this.sendRequest(POST, `/${actor}/`, TYPE_JSON, info)
   }
@@ -91,15 +93,25 @@ export class SocialInboxClient {
     return await this.deleteActor(this.account)
   }
 
-  async sendActorInbox (actor: string, activity: APActivity): Promise<void> {
-    await this.sendRequest(POST, `/${actor}/inbox`, TYPE_LDJSON, activity)
+  // Admins
+  async listAdmins (): Promise<string[]> {
+    const response = await this.sendRequest(GET, '/admins')
+    const text = await response.text()
+    return text.split(NEWLINE)
   }
 
+  async addAdmins (admins: string[]): Promise<void> {
+    await this.sendRequest(POST, '/admins', TYPE_TEXT, admins.join(NEWLINE))
+  }
+
+  async removeAdmins (admins: string[]): Promise<void> {
+    await this.sendRequest(DELETE, '/admins', TYPE_TEXT, admins.join(NEWLINE))
+  }
+
+  // blocklist
   async getGlobalBlocklist (): Promise<string[]> {
     const response = await this.sendRequest(GET, '/blocklist')
-
     const text = await response.text()
-
     return text.split(NEWLINE)
   }
 
@@ -109,5 +121,72 @@ export class SocialInboxClient {
 
   async removeGlobalBlocklist (list: string[]): Promise<void> {
     await this.sendRequest(POST, '/blocklist', TYPE_TEXT, list.join(NEWLINE))
+  }
+
+  // Allowlist
+  async getGlobalAllowlist (): Promise<string[]> {
+    const response = await this.sendRequest(GET, '/allowlist')
+    const text = await response.text()
+    return text.split(NEWLINE)
+  }
+
+  async addGlobalAllowlist (accounts: string[]): Promise<void> {
+    await this.sendRequest(POST, '/allowlist', TYPE_TEXT, accounts.join(NEWLINE))
+  }
+
+  async removeGlobalAllowlist (accounts: string[]): Promise<void> {
+    await this.sendRequest(DELETE, '/allowlist', TYPE_TEXT, accounts.join(NEWLINE))
+  }
+
+  // Followers
+  async listFollowers (actor: string): Promise<string[]> {
+    const response = await this.sendRequest(GET, `/${actor}/followers`)
+    return await response.json()
+  }
+
+  async removeFollower (actor: string, follower: string): Promise<void> {
+    await this.sendRequest(DELETE, `/${actor}/followers/${encodeURIComponent(follower)}`)
+  }
+
+  // Hooks
+  async getHook (actor: string, hookType: string): Promise<any> {
+    const response = await this.sendRequest(GET, `/${actor}/hooks/${hookType}`)
+    return await response.json()
+  }
+
+  async setHook (actor: string, hookType: string, hook: any): Promise<void> {
+    await this.sendRequest(PUT, `/${actor}/hooks/${hookType}`, TYPE_JSON, hook)
+  }
+
+  async deleteHook (actor: string, hookType: string): Promise<void> {
+    await this.sendRequest(DELETE, `/${actor}/hooks/${hookType}`)
+  }
+
+  // Inbox
+  async fetchInbox (actor: string): Promise<any> {
+    const response = await this.sendRequest(GET, `/${actor}/inbox`)
+    return await response.json()
+  }
+
+  async postToInbox (actor: string, activity: APActivity): Promise<void> {
+    await this.sendRequest(POST, `/${actor}/inbox`, TYPE_LDJSON, activity)
+  }
+
+  async approveInboxItem (actor: string, itemId: string): Promise<void> {
+    await this.sendRequest(POST, `/${actor}/inbox/${itemId}`)
+  }
+
+  async rejectInboxItem (actor: string, itemId: string): Promise<void> {
+    await this.sendRequest(DELETE, `/${actor}/inbox/${itemId}`)
+  }
+
+  // Outbox
+  async postToOutbox (actor: string, activity: APActivity): Promise<void> {
+    await this.sendRequest(POST, `/${actor}/outbox`, TYPE_LDJSON, activity)
+  }
+
+  async fetchOutboxItem (actor: string, itemId: string): Promise<APActivity> {
+    const response = await this.sendRequest(GET, `/${actor}/outbox/${itemId}`)
+    return await response.json()
   }
 }
