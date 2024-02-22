5687b4214b8ca66eb974231be9f0b54f67734ebc
Nulo <nulo@sutty.nl>
2024-02-22 13:17:10 -0300

    announcements: add endpoints for outbox

--- src/server/api/announcements.ts
+++ src/server/api/announcements.ts
@@ -3,6 +3,7 @@ import { APActor } from 'activitypub-types'
 import type { APIConfig, FastifyTypebox } from '.'
 import Store from '../store'
 import type ActivityPubSystem from '../apsystem'
+import { Type } from '@sinclair/typebox'
 
 type APActorNonStandard = APActor & {
   publicKey: {
@@ -49,5 +50,57 @@ export const announcementsRoutes = (cfg: APIConfig, store: Store, apsystem: Acti
         publicKeyPem: actor.keypair.publicKeyPem
       }
     })
+  })
+
+  server.get<{
+    // TODO: typebox APOrderedCollection
+    Reply: any
+  }>('/announcements/outbox', {
+    schema: {
+      params: {},
+      // XXX: even with Type.Any(), the endpoint returns `{}` :/
+      // response: {
+      //   // TODO: typebox APOrderedCollection
+      //   200: Type.Any()
+      // },
+      description: 'Announcements ActivityPub outbox',
+      tags: ['ActivityPub']
+    }
+  }, async (request, reply) => {
+    const actor = await store.announcements.getInfo()
+    const activities = await store.announcements.outbox.list()
+    const orderedItems = activities.map(a => a.id)
+    return await reply.send({
+      '@context': 'https://www.w3.org/ns/activitystreams',
+      id: `${actor.actorUrl}/outbox`,
+      type: 'OrderedCollection',
+      totalItems: orderedItems.length,
+      orderedItems
+    })
+  })
+
+  server.get<{
+    Params: {
+      id: string
+    }
+    // TODO: typebox APOrderedCollection
+    Reply: any
+  }>('/announcements/outbox/:id', {
+    schema: {
+      params: Type.Object({
+        id: Type.String()
+      }),
+      // XXX: even with Type.Any(), the endpoint returns `{}` :/
+      // response: {
+      //   // TODO: typebox APOrderedCollection
+      //   200: Type.Any()
+      // },
+      description: 'Announcements ActivityPub get activity',
+      tags: ['ActivityPub']
+    }
+  }, async (request, reply) => {
+    const actor = await store.announcements.getInfo()
+    const activity = await store.announcements.outbox.get(`${actor.actorUrl}/outbox/${request.params.id}`)
+    return await reply.send(activity)
   })
 }
