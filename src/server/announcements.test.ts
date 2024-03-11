b0d1dc9c29492ba350d4432df74458c4230ef65d
Nulo <nulo@sutty.nl>
2024-03-11 17:54:26 -0300

    chore: add test for announcements

---
+++ src/server/announcements.test.ts
@@ -0,0 +1,46 @@
+import test from 'ava'
+import sinon from 'sinon'
+import ActivityPubSystem, { FetchLike } from './apsystem'
+import Store from './store/index.js'
+import { ModerationChecker } from './moderation.js'
+import HookSystem from './hooksystem'
+import { MemoryLevel } from 'memory-level'
+
+// Create some mock dependencies
+const mockStore = new Store(new MemoryLevel())
+
+const mockModCheck = new ModerationChecker(mockStore)
+const mockFetch: FetchLike = async (input: RequestInfo | URL, init?: RequestInit): Promise<Response> => {
+  return new Response(JSON.stringify({}), { status: 200 })
+}
+const mockHooks = new HookSystem(mockStore, mockFetch)
+
+const aps = new ActivityPubSystem('http://localhost', mockStore, mockModCheck, mockHooks)
+
+test.beforeEach(async () => {
+  // Restore stubs before setting them up again
+  sinon.restore()
+
+  await aps.announcements.init()
+})
+
+const keypair = {
+  publicKeyPem: 'mockPublicKey',
+  privateKeyPem: 'mockPrivateKey',
+  publicKeyId: 'mockPublicKeyId'
+}
+
+test('actor gets announced on .announce', async t => {
+  const fakeActor = '@test@url'
+  const actorInfo = {
+    actorUrl: 'https://url/@test',
+    announce: true,
+    keypair,
+    publicKeyId: keypair.publicKeyId
+  }
+  await aps.store.forActor(fakeActor).setInfo(actorInfo)
+  await aps.announcements.announce(fakeActor, actorInfo)
+
+  const outbox = await aps.store.forActor('@announcements@localhost').outbox.list()
+  t.is(outbox.length, 1, 'something is in outbox')
+})
