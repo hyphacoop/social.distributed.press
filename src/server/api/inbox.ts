3ba3bd4649d94b9670ea3652830398b6b4a0579e
Nulo <nulo@sutty.nl>
2024-05-16 16:37:53 -0300

    feat: WIP: paginated inbox

--- src/server/api/inbox.ts
+++ src/server/api/inbox.ts
@@ -1,5 +1,5 @@
-import { APActivity, IdField, APOrderedCollection } from 'activitypub-types'
-import { Type } from '@sinclair/typebox'
+import { APActivity, IdField, APOrderedCollection, APOrderedCollectionPage } from 'activitypub-types'
+import { Static, Type } from '@sinclair/typebox'
 
 import type { APIConfig, FastifyTypebox } from '.'
 import Store from '../store'
@@ -7,6 +7,11 @@ import type ActivityPubSystem from '../apsystem.js'
 import createError from 'http-errors'
 
 export const inboxRoutes = (cfg: APIConfig, store: Store, apsystem: ActivityPubSystem) => async (server: FastifyTypebox): Promise<void> => {
+  const GetInboxQuerySchema = Type.Object({
+    skip: Type.Optional(Type.Number()),
+    limit: Type.Optional(Type.Number()),
+  })
+
   // Returns an JSON-LD OrderedCollection with items in the moderation queue
   // Follows / Boosts/ Replies / etc will all be mixed in here
   // Note that items will get auto-denied if they match a user / instance in the blocklist
@@ -15,31 +20,54 @@ export const inboxRoutes = (cfg: APIConfig, store: Store, apsystem: ActivityPubS
     Params: {
       actor: string
     }
-    Reply: APOrderedCollection | string
+    Querystring: Static<typeof GetInboxQuerySchema>
+    Reply: APOrderedCollectionPage | string
   }>('/:actor/inbox', {
     schema: {
       params: Type.Object({
         actor: Type.String()
       }),
+      querystring: GetInboxQuerySchema,
       description: 'Items in the inbox queue.',
       tags: ['ActivityPub']
     }
   }, async (request, reply) => {
     const { actor } = request.params
+    let { skip, limit } = request.query
 
     const allowed = await apsystem.hasPermissionActorRequest(actor, request)
     if (!allowed) {
       return await reply.code(403).send('Not Allowed')
     }
 
-    const orderedItems = await store.forActor(actor).inbox.list()
-    const orderedCollection: APOrderedCollection = {
-      '@context': 'https://www.w3.org/ns/activitystreams',
-      type: 'OrderedCollection',
-      id: `${cfg.publicURL}${request.url}`,
-      orderedItems
+    const inbox = store.forActor(actor).inbox
+    const totalItems = await inbox.count()
+
+    if (!limit) {
+      const page: APOrderedCollectionPage = {
+        '@context': 'https://www.w3.org/ns/activitystreams',
+        type: 'OrderedCollectionPage',
+        totalItems,
+        id: `${cfg.publicURL}${request.url}`,
+        next: `${cfg.publicURL}/v1/${actor}/inbox?limit=100`
+      }
+      return await reply.send(page)
     }
-    return await reply.send(orderedCollection)
+    skip ??= 0
+
+    const hasPrev = skip > limit
+    const prev = hasPrev ? `${cfg.publicURL}/v1/${actor}/inbox?skip=${skip - limit}&limit=${limit}` : undefined
+    const orderedItems = await inbox.list(skip, limit)
+    const orderedCollectionPage: APOrderedCollectionPage = {
+      '@context': 'https://www.w3.org/ns/activitystreams',
+      type: 'OrderedCollectionPage',
+      totalItems,
+      id: `${cfg.publicURL}${request.url}`,
+      prev,
+      next: `${cfg.publicURL}/v1/${actor}/inbox?skip=${skip + limit}&limit=${limit}`,
+      orderedItems,
+    }
+    return await reply.send(orderedCollectionPage)
   })
 
   // This is what instances will POST to in order to notify of follows/replies/etc
--- src/server/store/ActivityStore.ts
+++ src/server/store/ActivityStore.ts
@@ -1,5 +1,5 @@
 import { AbstractLevel } from 'abstract-level'
-import { APActivity } from 'activitypub-types'
+import { APActivity, PublishedField } from 'activitypub-types'
 import createError from 'http-errors'
 
 export class ActivityStore {
@@ -20,11 +20,31 @@ export class ActivityStore {
     }
     const key = this.urlToKey(activity.id)
     await this.db.put(key, activity)
+    await this.addToIndex(getPublished(activity.published), key)
   }
 
   async remove (url: string): Promise<void> {
     const key = this.urlToKey(url)
     await this.db.del(key)
+    await this.removeFromIndex(key)
+  }
+
+  get publishedIndex() {
+    return this.db
+      .sublevel('indexes', { valueEncoding: 'json' })
+      .sublevel('published', { valueEncoding: 'json' })
+  }
+
+  async addToIndex (publishedAt: Date, id: string): Promise<void> {
+    await this.publishedIndex.put(`${publishedAt.toISOString()}-${id}`, id)
+  }
+  async removeFromIndex (id: string): Promise<void> {
+    for await (const [key, value] of this.publishedIndex.iterator()) {
+      if (value === id) {
+        await this.publishedIndex.del(key)
+        break
+      }
+    }
   }
 
   async get (url: string): Promise<APActivity> {
@@ -37,11 +57,31 @@ export class ActivityStore {
     }
   }
 
-  async list (): Promise<APActivity[]> {
+  async list (skip: number = 0, limit: number = 999999): Promise<APActivity[]> {
     const activities: APActivity[] = []
-    for await (const value of this.db.values()) {
-      activities.push(value)
+    let skipped = 0
+    for await (const id of this.publishedIndex.values({ limit: limit + skip })) {
+      if (skipped < skip) {
+        skipped++
+        continue
+      }
+      activities.push(await this.db.get(id))
     }
     return activities
   }
+  
+  async count (): Promise<number> {
+    let count = 0
+    for await (const _ of this.db.keys()) {
+      count++
+    }
+    return count
+  }
+}
+
+function getPublished (published?: PublishedField): Date {
+  if (published) {
+    if (published instanceof Date) return published
+    else return new Date(published)
+  } else return new Date()
 }
