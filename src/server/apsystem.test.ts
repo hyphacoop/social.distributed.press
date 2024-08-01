fc50c6474e74f6e2b9455024b94a658b075325dd
Nulo <nulo@sutty.nl>
2024-08-01 17:29:45 -0300

    chore: add test for interacted store

--- src/server/apsystem.test.ts
+++ src/server/apsystem.test.ts
@@ -1,6 +1,6 @@
 import test from 'ava'
 import sinon from 'sinon'
-import ActivityPubSystem, { FetchLike, DEFAULT_PUBLIC_KEY_FIELD } from './apsystem'
+import ActivityPubSystem, { FetchLike, DEFAULT_PUBLIC_KEY_FIELD, parseMention } from './apsystem'
 import type { FastifyBaseLogger, FastifyRequest } from 'fastify'
 import Store from './store/index.js'
 import { ModerationChecker } from './moderation.js'
@@ -9,6 +9,7 @@ import signatureParser from 'activitypub-http-signatures'
 import { MemoryLevel } from 'memory-level'
 import { APActivity } from 'activitypub-types'
 import { MockFetch } from './fixtures/mockFetch.js'
+import { generateKeypair } from 'http-signed-fetch'
 
 // Helper function to create a new Store instance
 function newStore (): Store {
@@ -291,6 +292,94 @@ test('ActivityPubSystem - List shares', async t => {
   const collection = await aps.sharesCollection(actorMention, object)
 
   t.deepEqual(collection.items, [activity])
+})
+
+test('ActivityPubSystem - Interacted store', async t => {
+  const store = newStore()
+  const mockFetch = new MockFetch()
+  const hookSystem = new HookSystem(store, mockFetch.fetch as FetchLike)
+  const aps = new ActivityPubSystem(
+    'http://localhost',
+    store,
+    mockModCheck,
+    hookSystem,
+    mockLog,
+    mockFetch.fetch as FetchLike
+  )
+
+  const object = 'https://example.com/note1'
+  const authorMention = '@author@example.com'
+  const authorUrl = 'https://example.com/author'
+  // required for signed fetch
+  await store.forActor(authorMention).setInfo({
+    keypair: { ...generateKeypair() },
+    actorUrl: authorUrl,
+    publicKeyId: 'testAccount#main-key'
+  })
+
+  mockFetch.mockActor(authorMention)
+  mockFetch.set(object, JSON.stringify({
+    // '@context': 'https://www.w3.org/ns/activitystreams',
+    // type: 'Create',
+    // published: new Date().toISOString(),
+    // actor: 'https://example.com/author',
+    // object: {
+    '@context': 'https://www.w3.org/ns/activitystreams',
+    type: 'Note',
+    published: new Date().toISOString(),
+    content: 'Hello world',
+    to: [
+      'https://example.com/author/followers'
+    ],
+    cc: [
+      'https://www.w3.org/ns/activitystreams#Public'
+    ],
+    id: object,
+    attributedTo: authorUrl
+    // },
+    // id: 'https://example.com/activity1'
+  }))
+  const actorMentions = [
+    '@user1@example1.com',
+    '@user2@example2.com',
+    '@user3@example3.com'
+  ]
+
+  for (let i = 0; i < actorMentions.length; i++) {
+    const actorMention = actorMentions[i]
+    const { username, domain } = parseMention(actorMention)
+    // Sample data for the tests
+    const activity: APActivity = {
+      '@context': 'https://www.w3.org/ns/activitystreams',
+      type: 'Like',
+      published: new Date().toISOString(),
+      actor: `https://${domain}/actor/${username}/`,
+      object,
+      id: `https://${domain}/activity${i}`
+    }
+    await store.forActor(authorMention).inbox.add(activity)
+    mockFetch.mockActor(actorMention)
+    await aps.approveActivity(authorMention, activity.id as string)
+  }
+
+  t.deepEqual(await store.forActor(authorMention).interacted.list(), actorMentions)
+
+  for (const actorMention of actorMentions) {
+    const { username, domain } = parseMention(actorMention)
+    mockFetch.set(`https://${domain}/actor/${username}/inbox`, '')
+  }
+  await aps.notifyInteracted(authorMention, {
+    '@context': 'https://www.w3.org/ns/activitystreams',
+    type: 'Delete',
+    published: new Date().toISOString(),
+    actor: authorUrl,
+    object,
+    id: 'https://example.com/activity2'
+  })
+  for (const actorMention of actorMentions) {
+    const { username, domain } = parseMention(actorMention)
+    t.assert(mockFetch.history.includes(`https://${domain}/actor/${username}/inbox`))
+  }
 })
 
 // After all tests, restore all sinon mocks
